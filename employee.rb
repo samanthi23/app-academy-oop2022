a88b6f37ef824dfa388dfcff92a1f00388e322ef
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2022-05-01 22:42:03 +0000

    added initialize

--- employee.rb
+++ employee.rb
@@ -2,8 +2,20 @@ class Employee
     attr_reader :name, :title, :salary
     attr_accessor :boss
 
+    def initialize(name, title, salary, boss = nil)
+        @name = name
+        @title = title
+        @salary = salary
+        self.boss = boss
+    end
+
 end
 
 class Manager < Employee
     attr_reader :employees
+    
+    def initialize(name, title, salary, boss = nil)
+        super(name,title,salary,boss)
+        employees = []
+    end
 end
