4ba2a6a41e00374de447353e2b33f9a997ca60d6
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2022-05-01 22:57:34 +0000

    OOP employee manager

--- employee.rb
+++ employee.rb
@@ -8,6 +8,18 @@ class Employee
         @salary = salary
         self.boss = boss
     end
+    
+    def boss=(boss)
+        @boss = boss
+        boss.add_employee(self) unless boss.nil?
+        
+    return boss
+        
+    end
+    
+    def bonus(multiplier)
+        self.salary * multiplier
+    end
 
 end
 
@@ -17,5 +29,33 @@ class Manager < Employee
     def initialize(name, title, salary, boss = nil)
         super(name,title,salary,boss)
         employees = []
+    end
+    
+    def add_employee(subordinate)
+       employees << subordinate 
+       
+       subordinate 
+    end
+    
+    def bonus(multiplier)
+       self.total_subsalary * multiplier
+       
+        
+    end
+    
+    protected
+    
+    def total_subsalary
+    total_subsalary = 0
+    self.employees.each do |employee |
+       if employee.is_a?(Manager) 
+           total_subsalary += employee.salary + employee.total_subsalary
+       else
+           total_subsalary += employee.salary
+       end
+           
+        end
+    return total_subsalary
+    
     end
 end
